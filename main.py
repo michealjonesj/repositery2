6e32968733e309e2dc1f02f0ba9d82b924a592c9
michealjonesj <jonesmich1817@gmail.com>
2024-08-02 15:59:01 +0530

    updated main.py

--- main.py
+++ main.py
@@ -2,10 +2,6 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/books/default")
-async def hello(default):
-    return ["Python Programming Language"]
-
-@app.get("/books/{book_name}/{book_id}")
-async def books(book_name: str, book_id: int):
-    return {"Books: bookname:" + book_name + "book_id:" + book_id }
+@app.delete("/items/{item_id}")
+def delete_item(item_id : int):
+    return {"message": f"Item {item_id} deleted"}
