05033ab29eda142d1aac9544e8b125101e9fab07
michealjonesj <jonesmich1817@gmail.com>
2024-08-08 12:13:11 +0530

    Updated request.py

---
+++ request.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+class UserData(BaseModel):
+    name : str
+    id : int
+    about : str = None
+
+app = FastAPI()
+@app.post('/user/')
+async def create_data(user_data : UserData):
+    return user_data
+    
